2960c650ca7874f07656335f57a66b50299d6f04
Sayak <sayak649@gmail.com>
2021-05-30 00:09:16 +0530

    okay

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "../styles/home.css";
 
 const Home = () => {
-  return <div>homepage</div>;
+  return (
+    <div
+      style={{
+        height: "90vh",
+        width: "100vw",
+        backgroundColor: "blue",
+        textAlign: "center",
+        paddingTop: "30vh",
+      }}
+    >
+      <h1>Welcome to TASKS App</h1>
+    </div>
+  );
 };
 
 export default Home;
